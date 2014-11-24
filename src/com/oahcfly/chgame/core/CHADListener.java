1d7ff2bc55eb1316e4bb2b3ee18c36ea8014fa59
caohao <caoshen1990@163.com>
2014-11-24 12:57:49 +0800

    添加广告接口

---
+++ src/com/oahcfly/chgame/core/CHADListener.java
@@ -0,0 +1,27 @@
+package com.oahcfly.chgame.core;
+
+public interface CHADListener {
+
+    /**
+     * 
+     * <pre>
+     * 显示插屏广告
+     * 
+     * date: 2014-11-24
+     * </pre>
+     * @author caohao
+     */
+    public void showSpotAds();
+ 
+    /**
+     * 
+     * <pre>
+     * 广告是否开启
+     * 
+     * date: 2014-11-24
+     * </pre>
+     * @author caohao
+     * @return
+     */
+    public boolean isOpenAD();
+}
