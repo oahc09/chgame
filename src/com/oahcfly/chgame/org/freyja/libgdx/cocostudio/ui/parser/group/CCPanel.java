e932c0a8ecd59b20d47f1cdfdd76e6787440d16c
caohao <caoshen1990@163.com>
2014-11-25 13:13:59 +0800

    Fix:Panel背景色不生效的问题；

--- src/com/oahcfly/chgame/org/freyja/libgdx/cocostudio/ui/parser/group/CCPanel.java
+++ src/com/oahcfly/chgame/org/freyja/libgdx/cocostudio/ui/parser/group/CCPanel.java
@@ -1,3 +1,4 @@
+
 package com.oahcfly.chgame.org.freyja.libgdx.cocostudio.ui.parser.group;
 
 import com.badlogic.gdx.graphics.Pixmap;
@@ -21,55 +22,46 @@ import com.oahcfly.chgame.org.freyja.libgdx.cocostudio.ui.parser.GroupParser;
  */
 public class CCPanel extends GroupParser {
 
-	@Override
-	public String getClassName() {
-		return "Panel";
-	}
+    @Override
+    public String getClassName() {
+        return "Panel";
+    }
 
-	@Override
-	public Actor parse(CocoStudioUIEditor editor, CCWidget widget,
-			CCOption option) {
-		Table table = new Table();
+    @Override
+    public Actor parse(CocoStudioUIEditor editor, CCWidget widget, CCOption option) {
+        Table table = new Table();
 
-		if (option.getColorType() == 0) {// 无颜色
+        if (option.getColorType() == 0) {// 无颜色
 
-		} else if (option.getColorType() == 1) {// 单色
+        } else if (option.getColorType() == 1) {// 单色
 
-			Pixmap pixmap = new Pixmap((int) option.getWidth(),
-					(int) option.getHeight(), Format.RGBA8888);
-			pixmap.setColor(option.getBgColorR() / 255f,
-					option.getBgColorG() / 255f, option.getBgColorB() / 255f,
-					option.getBgColorOpacity() / 255f);
+            Pixmap pixmap = new Pixmap((int)option.getWidth(), (int)option.getHeight(), Format.RGBA8888);
+            pixmap.setColor(option.getBgColorR() / 255f, option.getBgColorG() / 255f, option.getBgColorB() / 255f,
+                    option.getBgColorOpacity());
+            pixmap.fill();
 
-			pixmap.fill();
+            table.setBackground(new TextureRegionDrawable(new TextureRegion(editor.getTexture(pixmap))));
+            pixmap.dispose();
+        } else {// 渐变色
 
-			table.setBackground(new TextureRegionDrawable(new TextureRegion(
-					editor.getTexture(pixmap))));
-			pixmap.dispose();
-		} else {// 渐变色
+        }
 
-		}
+        if (option.getBackGroundImageData() != null) {// Panel的图片并不是拉伸平铺的!!.但是这里修改为填充
+            Drawable tr = editor.findDrawable(option, option.getBackGroundImageData().getPath());
+            if (tr != null) {
+                Image bg = new Image(tr);
+                bg.setPosition((option.getWidth() - bg.getWidth()) / 2, (option.getHeight() - bg.getHeight()) / 2);
+                // bg.setFillParent(true);
+                bg.setTouchable(Touchable.disabled);
 
-		if (option.getBackGroundImageData() != null) {// Panel的图片并不是拉伸平铺的!!.但是这里修改为填充
-			Drawable tr = editor.findDrawable(option, option
-					.getBackGroundImageData().getPath());
-			if (tr != null) {
-				Image bg = new Image(tr);
-				bg.setPosition((option.getWidth() - bg.getWidth()) / 2,
-						(option.getHeight() - bg.getHeight()) / 2);
-				// bg.setFillParent(true);
-				bg.setTouchable(Touchable.disabled);
+                bg.setColor(option.getColorR() / 255f, option.getColorG() / 255f, option.getColorB() / 255f,
+                        option.getOpacity());
+                table.addActor(bg);
+            }
+        }
 
-				bg.setColor(option.getColorR() / 255f,
-						option.getColorG() / 255f, option.getColorB() / 255f,
-						option.getOpacity() / 255f);
-				table.addActor(bg);
-			}
-		}
+        table.setClip(option.isClipAble());
 
-		table.setClip(option.isClipAble());
-
-		return table;
-	}
-
+        return table;
+    }
 }
