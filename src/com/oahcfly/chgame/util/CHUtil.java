58d429d80a2b096adc24e821fd7bfb2f9fd051a8
caohao <caoshen1990@163.com>
2014-11-25 15:07:14 +0800

    util

--- src/com/oahcfly/chgame/util/CHUtil.java
+++ src/com/oahcfly/chgame/util/CHUtil.java
@@ -1,15 +1,8 @@
 
 package com.oahcfly.chgame.util;
 
-import com.badlogic.gdx.scenes.scene2d.Action;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 
-public class CHUtils {
-
-    public static Action getClickAction() {
-        Action mClick = Actions.sequence(Actions.alpha(0.5f, 0.1f), Actions.alpha(1f, 0.1f));
-        return mClick;
-    }
+public class CHUtil {
 
     public static String getTimeFormat(int time) {
         String str = "";
