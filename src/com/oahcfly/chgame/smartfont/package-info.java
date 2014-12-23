dc006a934fe513a9d3e1a48480418a770d0ec38a
caohao <caoshen1990@163.com>
2014-12-23 16:00:37 +0800

    集成SmartFont

---
+++ src/com/oahcfly/chgame/smartfont/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Project Name:CHGame-core
+ * Copyright (c) 2014 OahcFly All Rights Reserved.
+ */
+/**
+ * <pre>
+ * 集成自https://github.com/jrenner/gdx-smart-font
+ * 
+ * 可以根据屏幕自动调整字体大小，并且缓存成文件
+ * date: 2014-12-23
+ * </pre>
+ * @author caohao
+ */
+
+package com.oahcfly.chgame.smartfont;
